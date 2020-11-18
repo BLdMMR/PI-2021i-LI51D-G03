ad8c97315fdd03bcd5214f3abaa02190381aabd0
EmptyPuff <bernardo.mira98@gmail.com>
2020-11-18 22:53:21 +0000

    First Code

---
+++ covida/covida-server.js
@@ -0,0 +1,19 @@
+'use strict'
+const PORT = 1906 || process.argv[2]
+
+const express = require('express')
+const web_api = require('./covida-web-api')
+
+const app = express()
+
+app.get('/api/', web_api.getMostPopularGames)
+app.get('/api/:name', web_api.getGameByName)
+app.get('/api/groups', web_api.getAllGroups)
+app.get('/api/groups/:groupid', web_api.getGroupInfo)
+app.get('/api/groups/:groupid/:total_rating', web_api.getGamesFromGroupBasedOnRating)
+app.post('/api/groups', web_api.createGroup)
+app.put('/api/groups/:id', web_api.addGameToGroup)
+app.put('/api/groups/:id', web_api.removeGameFromGroup)
+app.put('/api/groups/:id', web_api.updateGroup)
+
+app.listen(PORT)
---
+++ covida/covida-web-api.js
@@ -0,0 +1,41 @@
+'use strict'
+
+module.exports = {
+    getMostPopularGames: getMostPopularGames(),
+    getGameByName: getGameByName(),
+    getAllGroups: getAllGroups(),
+    getGroupInfo: getGroupInfo(),
+    getGamesFromGroupBasedOnRating: getGamesFromGroupBasedOnRating(),
+    createGroup: createGroup(),
+    addGameToGroup: addGameToGroup(),
+    removeGameFromGroup: removeGameFromGroup(),
+    updateGroup: updateGroup()
+}
+
+function getMostPopularGames(req, rsp) {
+
+}
+function getGameByName(req, rsp) {
+
+}
+function getAllGroups(req, rsp) {
+
+}
+function getGroupInfo(req, rsp) {
+
+}
+function getGamesFromGroupBasedOnRating(req, rsp) {
+
+}
+function createGroup(req, rsp) {
+
+}
+function addGameToGroup(req, rsp) {
+
+}
+function removeGameFromGroup(req, rsp) {
+
+}
+function updateGroup(req, rsp) {
+
+}
