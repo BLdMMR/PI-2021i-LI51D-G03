2a8a462c74826fcddec44740b1d81760f77bc2e3
EmptyPuff <bernardo.mira98@gmail.com>
2020-11-19 02:41:17 +0000

    Key Created and Authentication Done

--- covida/covida-web-api.js
+++ covida/covida-web-api.js
@@ -1,41 +1,59 @@
 'use strict'
 
 module.exports = {
-    getMostPopularGames: getMostPopularGames(),
-    getGameByName: getGameByName(),
-    getAllGroups: getAllGroups(),
-    getGroupInfo: getGroupInfo(),
-    getGamesFromGroupBasedOnRating: getGamesFromGroupBasedOnRating(),
-    createGroup: createGroup(),
-    addGameToGroup: addGameToGroup(),
-    removeGameFromGroup: removeGameFromGroup(),
-    updateGroup: updateGroup()
+    getMostPopularGames: getMostPopularGames,
+    getGameByName: getGameByName,
+    getAllGroups: getAllGroups,
+    getGroupInfo: getGroupInfo,
+    getGamesFromGroupBasedOnRating: getGamesFromGroupBasedOnRating,
+    createGroup: createGroup,
+    addGameToGroup: addGameToGroup,
+    removeGameFromGroup: removeGameFromGroup,
+    updateGroup: updateGroup
 }
 
 function getMostPopularGames(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("getMostPopularGames");
 }
 function getGameByName(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("getGameByName");
 }
 function getAllGroups(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("getAllGroups");
 }
 function getGroupInfo(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("getGroupInfo");
 }
 function getGamesFromGroupBasedOnRating(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("getGamesFromGroupBasedOnRating");
 }
 function createGroup(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("createGroup");
 }
 function addGameToGroup(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("addGameToGroup");
 }
 function removeGameFromGroup(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("removeGameFromGroup");
 }
 function updateGroup(req, rsp) {
-
+    rsp
+        .status(200)
+        .end("updateGroup");
 }
---
+++ covida/igdb-data.js
@@ -0,0 +1,3 @@
+const key = "2ss03ig2he3sv5zhuea9r5bgrv3pcn"
+const user_for_whaterver_purpose = "kul61h0ut19avpfl07q590hsgl3b3y"
+
